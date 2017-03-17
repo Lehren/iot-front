6cb315fa3cb4fded2b9929a099e36cba2a8d2ac2
lehren <loek-ehren@hotmail.com>
2017-03-17 14:12:45 +0100

    feat: dynamically add progress bars
    Add as many progress bars to the index.html file as are returned
    by the backend.

--- src/js/components/progress-component.js
+++ src/js/components/progress-component.js
@@ -1,3 +1,33 @@
-/**
- * Created by loek on 15-3-17.
- */
+global.jQuery = require('jquery');
+const $ = global.jQuery;
+
+module.exports = class ProgressComponent {
+
+  constructor(containerId){
+    this.containerId = containerId;
+  }
+
+  /**
+   * Draw
+   * <div class="progress vertical bottom" >
+   *    <div class="progress-bar" role="progressbar" data-transitiongoal="0"></div>
+   * </div>
+   */
+  draw(){
+    const rowContainer = document.createElement('div');
+    rowContainer.className = "row progress-row";
+
+    const progressContainer = document.createElement('div');
+    progressContainer.className = "progress vertical bottom";
+
+    const progressBar = document.createElement('div');
+    progressBar.id = "iot-container-" + this.containerId;
+    progressBar.className = "progress-bar";
+    progressBar.role = "progressbar";
+    $(progressBar).attr('data-transitiongoal', 0);
+
+    rowContainer.appendChild(progressContainer);
+    progressContainer.appendChild(progressBar);
+    return rowContainer;
+  }
+};
