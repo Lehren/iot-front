f8144842c420738d8fb20b6902e80a0c3f990637
lehren <loek-ehren@hotmail.com>
2017-03-17 14:32:06 +0100

    feat: enable offline functionality

--- src/js/index.js
+++ src/js/index.js
@@ -10,17 +10,34 @@ const ProgressComponent = require('./components/progress-component');
 const conn = new iot.Connection(settings);
 const fillLevelHandler = new iot.FillLevelController(conn);
 const containerDict = {};
-
+const cont1 = {
+  id: "1",
+  fillLevel: 20
+};
+const cont2= {
+  id: "2",
+  fillLevel: 10
+};
+const cont3 ={
+  id: "3",
+  fillLevel: 50
+};
+const containers = [cont1,cont2,cont3];
 document.addEventListener('DOMContentLoaded', () => {
   drawContainers();
-
   setInterval(updateContainers, 5000);
-
+  setInterval(() =>{
+    if(cont1.fillLevel >= 90){cont1.fillLevel = 0}
+    if(cont2.fillLevel >= 90){cont2.fillLevel = 0}
+    if(cont3.fillLevel >= 90){cont3.fillLevel = 0}
+    cont1.fillLevel = cont1.fillLevel + 5;
+    cont2.fillLevel += 8;
+    cont3.fillLevel += 3;
+  }, 3000);
 });
 
 function drawContainers() {
-  fillLevelHandler.getFillLevels()
-    .then(fillLevel => fillLevel.map(v => {
+  containers.map(v => {
       const containerClass = new ProgressComponent(v.id);
       const drawnContainer = containerClass.draw();
       const bootstrapBar = $(drawnContainer.firstChild.firstChild);
@@ -30,15 +47,14 @@ function drawContainers() {
       $(drawnContainer).append(IDtext);
       $("#content").append(drawnContainer);
       updateContainers();
-    }));
+    });
 }
 
 function updateContainers() {
-  return fillLevelHandler.getFillLevels()
-    .then(fillLevel => fillLevel.map(v => {
+  return containers.map(v => {
       const bar = $(containerDict[v.id]);
       bar.attr('data-transitiongoal', v.fillLevel).progressbar({display_text: 'center'});
-    }));
+    });
 }
 
 
