e85e350016d659230774a44ab304450b48af9b23
lehren <loek-ehren@hotmail.com>
2017-03-27 12:21:29 +0200

    feat: add colors from color map
    Add color levels from any arbitrary color map.

--- src/js/index.js
+++ src/js/index.js
@@ -31,22 +31,41 @@ function drawContainers() {
 
 function updateContainers() {
   return fillLevelHandler.getFillLevels()
-    .then(fillLevel => fillLevel.map(v => {
-      const bar = $(containerDict[v.id]);
-      let className;
-      if(v.fillLevel < 50){
-        className = "progress-bar-success";
-      }
-      else if(v.fillLevel >=50 && v.fillLevel <80){
-        className = "progress-bar-warning";
-      }
-      else if(v.fillLevel >=80){
-        className = "progress-bar-error";
-      }
-      bar.removeClass();
-      bar.addClass("progress-bar " + className);
-      bar.attr('data-transitiongoal', v.fillLevel).progressbar({display_text: 'center'});
-    }));
+    .then(data => {
+      data.map(dataEntry => {
+        const bar = $(containerDict[dataEntry.id]);
+        bar.css('background-color', getColorForPercentage(dataEntry.fillLevel / 100));
+        bar.attr('data-transitiongoal', dataEntry.fillLevel).progressbar({display_text: 'center'});
+      });
+      fillJsonPanel($("#json-panel"), data);
+    });
+}
+
+const percentColors = [
+  {pct: 0.0, color: {r: 0, g: 255, b: 0}},
+  {pct: 0.5, color: {r: 255, g: 255, b: 0}},
+  {pct: 1.0, color: {r: 255, g: 0, b: 0}}];
+
+function getColorForPercentage(pct) {
+  let i;
+  for (i = 1; i < percentColors.length - 1; i++) {
+    if (pct < percentColors[i].pct) {
+      break;
+    }
+  }
+  const lower = percentColors[i - 1];
+  const upper = percentColors[i];
+  const range = upper.pct - lower.pct;
+  const rangePct = (pct - lower.pct) / range;
+  const pctLower = 1 - rangePct;
+  const pctUpper = rangePct;
+  const color = {
+    r: Math.floor(lower.color.r * pctLower + upper.color.r * pctUpper),
+    g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
+    b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper)
+  };
+  return 'rgb(' + [color.r, color.g, color.b].join(',') + ')';
+  // or output as hex if preferred
 }
 
 
